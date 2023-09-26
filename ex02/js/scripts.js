6870297f83774032f81d848bc6e09e17d055b9e4
Bruno Vieira <133446390+brunovcunha@users.noreply.github.com>
2023-09-25 22:29:10 -0300

    Add files via upload

---
+++ ex02/js/scripts.js
@@ -0,0 +1,30 @@
+// function aumentarVotos(nroCand) {
+//     document.getElementById(`votosCand${nroCand}`).innerHTML++;
+// }
+
+document.getElementById("votosCand1").innerHTML=0;
+document.getElementById("votosCand2").innerHTML=0;
+document.getElementById("votosCand3").innerHTML=0;
+document.getElementById("votosCand4").innerHTML=0;
+
+vetorVotos = [0,0,0,0];
+
+fotoCand1 = document.getElementById("fotoCand1");
+fotoCand2 = document.getElementById("fotoCand2");
+fotoCand3 = document.getElementById("fotoCand3");
+fotoCand4 = document.getElementById("fotoCand4");
+
+
+// fotoCand1 = document.addEventListener("fotoCand2", addVoto);
+// fotoCand2 = document.addEventListener("fotoCand2", addVoto);
+// fotoCand3 = document.addEventListener("fotoCand3", addVoto);
+// fotoCand4 = document.addEventListener("fotoCand4", addVoto);
+
+fotoCand1.addEventListener("click", function(){addVotoCand(1)});
+fotoCand2.addEventListener("click", function(){addVotoCand(2)});
+fotoCand3.addEventListener("click", function(){addVotoCand(3)});
+fotoCand4.addEventListener("click", function(){addVotoCand(4)});
+
+function addVotoCand(nroCand){
+    document.getElementById(`votosCand${nroCand}`).innerHTML = ++vetorVotos[nroCand - 1];
+}
